fe8c388e1e9c2a57e0e64992b16b4129d03ecff6
quonaro <quonaro@mail.ru>
2023-09-10 23:26:52 +0700

    fix bug

--- install-submodules.py
+++ install-submodules.py
@@ -2,9 +2,8 @@ import os
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-
 submodule_folder_names = os.listdir(f'{current_dir}/git_submodules/')
 for submodule_folder_name in submodule_folder_names:
     # Установка модулей и их локальных зависимостей
-    os.system(f'pip install {current_dir}/git_submodules/{submodule_folder_name}')
+    os.system(f'pip install {parent_dir}/git_submodules/{submodule_folder_name}')
     
