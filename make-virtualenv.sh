7ee91a5ce32ca397f85764b77a76c900d77b8696
quonaro <quonaro@mail.ru>
2023-11-05 18:27:20 +0700

    delete install virtualenv

--- make-virtualenv.sh
+++ make-virtualenv.sh
@@ -1,26 +1,3 @@
-for mngr in pacman apt yum zypp apt-get apk; do
-    which $mngr >/dev/null 2>&1
-    if [ $? -eq 0 ]
-    then
-        pkgmngr=$i
-    fi
-done
-
-
-python -m virtualenv >/dev/null 2>&1
-if [ $? -eq 2 ];
-then
-    echo "Виртуальное окружение установлено"
-else
-    echo Установка Virtualenv...
-    if [[ $pkgmngr -eq "pacman" ]]
-    then
-        sudo pacman -S python-virtualenv 
-    else
-        python3 -m pip install virtualenv
-    fi
-fi
-
 echo Создание виртуального окружения...
 python3 -m virtualenv .venv >/dev/null 2>&1
 
