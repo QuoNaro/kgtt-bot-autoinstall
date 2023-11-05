e2f66c35aeaa0ea155e6ff908c2e11f1e793ba13
quonaro <quonaro@mail.ru>
2023-11-05 19:12:04 +0700

    add check_venv

--- make-virtualenv.sh
+++ make-virtualenv.sh
@@ -1,6 +1,8 @@
-
-echo Создание виртуального окружения...
-python3 -m virtualenv .venv >/dev/null 2>&1
+ls .venv >/dev/null 2>&1
+if [ $? -ne 0 ];then
+    echo Создание виртуального окружения...
+    python3 -m virtualenv .venv >/dev/null 2>&1
+fi
 
 echo Установка зависимостей проекта...
 ./.venv/bin/python -m pip install -r ./kgtt-bot-autoinstall/requirements.txt >/dev/null 2>&1
