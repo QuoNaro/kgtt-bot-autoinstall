7ada91bb692f74e95d99c3a3c71801f94ef0f8d2
quonaro <quonaro@mail.ru>
2023-11-05 15:38:11 +0700

    links Instead of paths

--- kgtt.sh
+++ kgtt.sh
@@ -14,11 +14,11 @@ git clone https://github.com/QuoNaro/kgtt-bot.git >/dev/null 2>&1
 
 
 # Создание виртуального окружения
-bash ./make-virtualenv.sh
+bash https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-virtualenv.sh
 
 
 echo Установка зависимостей проекта...
 pip install -r https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/requirements.txt >/dev/null 2>&1
 
 # Создание конфига
-bash ./make-config.sh
+bash https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-config.sh
