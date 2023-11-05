182287dd58b18d14318823fe37d2a1eb4c21c911
quonaro <quonaro@mail.ru>
2023-11-05 15:42:32 +0700

    update

--- kgtt.sh
+++ kgtt.sh
@@ -14,11 +14,11 @@ git clone https://github.com/QuoNaro/kgtt-bot.git >/dev/null 2>&1
 
 
 # Создание виртуального окружения
-curl - SL https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-virtualenv.sh | bash -
+curl -SL https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-virtualenv.sh | bash -
 
 
 echo Установка зависимостей проекта...
 pip install -r https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/requirements.txt >/dev/null 2>&1
 
 # Создание конфига
-curl - SL https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-config.sh | bash -
+curl -SL https://raw.githubusercontent.com/QuoNaro/kgtt-bot-autoinstall/main/make-config.sh | bash -
