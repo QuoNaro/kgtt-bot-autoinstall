3de2d6e86341c5f0944268268721023dfa9db1ee
quonaro <quonaro@mail.ru>
2023-11-05 16:19:14 +0700

    update

--- make-config.sh
+++ make-config.sh
@@ -1,39 +1,12 @@
-echo -n "Создать конфиг-файл ? [Y/n] "
-read item
-case "$item" in
-    y|Y) {
-
-cd kgtt-bot/src
-read -p 'Введите токен для бота : ' token
-
-read -p 'Введите ID гугл таблицы : ' sheet
-if [[ $sheet = "" ]]
-    then 
-    sheet='1rGJ4_4BbSm0qweN7Iusz8d55e6uNr6bFRCv_j3W5fGU'
-fi
-
-read -p 'Введите таймаут рассылки (в секундах) : ' reltime
-if [[ $reltime = "" ]]
-    then 
-    reltime=120
-fi
-
-read -p 'Введите полный путь до базы данных : ' dbpath
-if [[ $dbpath = "" ]]
-    then 
-    dbpath=$(pwd)/kgtt.sqlite
-fi
-
+sheet='1rGJ4_4BbSm0qweN7Iusz8d55e6uNr6bFRCv_j3W5fGU'
+reltime=120
+dbpath=$(pwd)/kgtt.sqlite
 echo "config = {
 'token'             : '$token'      ,
 'table-id'          : '$sheet'      ,
 'table-reload-time' :  $reltime     ,
 'db-path'           : '$dbpath'     ,  
-} " > config.py
+} " > ./kgtt-bot/src/config.py
 
-cd ../..
-    };;
-
-    n|N) ;;
-    *);;
-esac
+path=$(pwd)/kgtt-bot/src/config.py
+echo Создан стандартный конфиг для бота [ $path ] 
