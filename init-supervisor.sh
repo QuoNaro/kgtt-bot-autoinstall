6f0e8110f799236c111b72649deeeb73a3fbdbd5
quonaro <quonaro@mail.ru>
2023-11-05 18:54:16 +0700

    update

--- init-supervisor.sh
+++ init-supervisor.sh
@@ -1,11 +1,13 @@
 pyenv=$(pwd)/.venv/bin/python
 supervisor_dir='/etc/supervisor/conf.d/'
 
+
 for name in bot mail; do
 echo "
 [program:$name]
 command=$pyenv $(pwd)/kgtt-bot/src/$name-start.py
 autostart=true
 autorestart=true
+stdout_logfile= $(pwd)/logs/$name.log
 " > $supervisor_dir$name.conf
 done
--- make-virtualenv.sh
+++ make-virtualenv.sh
@@ -1,10 +1,8 @@
+
 echo Создание виртуального окружения...
 python3 -m virtualenv .venv >/dev/null 2>&1
 
-echo Активация виртуального окружения...
-source ./.venv/bin/activate >/dev/null 2>&1
-
 echo Установка зависимостей проекта...
-python3 -m pip install -r ./kgtt-bot-autoinstall/requirements.txt >/dev/null 2>&1
+./.venv/bin/python -m pip install -r ./kgtt-bot-autoinstall/requirements.txt >/dev/null 2>&1
 
 
