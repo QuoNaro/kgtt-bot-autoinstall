94b4f20515b4a528314fb6e417729190f13205a6
quonaro <quonaro@mail.ru>
2023-11-05 15:35:12 +0700

    delete old files

--- install-submodules.py
+++
@@ -1,9 +0,0 @@
-import os
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-submodule_folder_names = os.listdir(f'{parent_dir}/git_submodules/')
-for submodule_folder_name in submodule_folder_names:
-    # Установка модулей и их локальных зависимостей
-    os.system(f'pip install {parent_dir}/git_submodules/{submodule_folder_name}')
-    
