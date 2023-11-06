17fd9fe6b1ad8a4e08160687aa18c5eb7620e9a2
quonaro <quonaro@mail.ru>
2023-11-06 13:46:38 +0700

    update

--- init-supervisor.sh
+++ init-supervisor.sh
@@ -3,11 +3,10 @@ supervisor_dir='/etc/supervisor/conf.d/'
 
 
 for name in bot mail; do
-echo "
-[program:$name]
+echo "[program:$name]
 command=$pyenv $(pwd)/kgtt-bot/src/$name-start.py
 autostart=true
 autorestart=true
-stdout_logfile= $(pwd)/logs/$name.log
+stdout_logfile=$(pwd)/logs/$name.log
 " > $supervisor_dir$name.conf
 done
