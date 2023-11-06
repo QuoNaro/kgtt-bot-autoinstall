c844a7f830b7d0ddd168e8a25b22d90b2b452907
quonaro <quonaro@mail.ru>
2023-11-06 12:32:02 +0700

    add text

--- make-empty-db.sh
+++ make-empty-db.sh
@@ -2,5 +2,6 @@ dbpath=$(pwd)/kgtt.sqlite
 
 cp $dbpath $dbpath >/dev/null 2>&1
 if [ $? -ne 0 ];then
+    echo База данных не найдена. Создание пустой базы данных...
     sqlite3 $dbpath < $(pwd)/kgtt-bot-autoinstall/db.sql >/dev/null 2>&1
 fi
